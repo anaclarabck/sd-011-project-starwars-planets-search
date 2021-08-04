ad7d6080d58f08ecfd66c48591d7a31444dab588
igordmf <igordanielmf@gmail.com>
2021-08-04 01:12:07 -0300

    Iniciando requisito 6

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import Provider from './context/Provider';
 import Table from './components/Table';
 import Header from './components/Header';
-import './App.css';
 import FiltersList from './components/FiltersList';
+import './App.css';
+import OrderSelector from './components/OrderSelector';
 
 function App() {
   return (
     <Provider>
       <main>
         <Header />
+        <OrderSelector />
         <FiltersList />
         <Table />
       </main>
---
+++ src/components/OrderSelector.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function OrderSelector() {
+  return (
+    <div>
+      OrderSelector
+    </div>
+  );
+}
+
+export default OrderSelector;
